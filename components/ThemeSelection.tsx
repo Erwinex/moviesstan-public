9ea632273bbce9d99dc842f769a269d6e64fdad0
Erwin <147875779+Erwinex@users.noreply.github.com>
2024-04-12 21:17:22 +0330

    A bit of styling

--- components/ThemeSelection.tsx
+++ components/ThemeSelection.tsx
@@ -1,27 +1,32 @@
 "use client";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 export default function ThemeSelection() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMouted] = useState(false);
+  useEffect(() => setMouted(true), []);
   return (
-    <div>
-      <select
-        className="rounded-full cursor-pointer inline-block sm:hidden"
-        value={theme}
-        onChange={(e) => setTheme(e.target.value)}
-      >
-        <option value="system">&#127763;</option>
-        <option value="dark">&#127761;</option>
-        <option value="light">&#127765;</option>
-      </select>
-      <select
-        className="rounded-md cursor-pointer hidden sm:inline-block"
-        value={theme}
-        onChange={(e) => setTheme(e.target.value)}
-      >
-        <option value="system">&#127763; System</option>
-        <option value="dark">&#127761; Dark</option>
-        <option value="light">&#127765; Light</option>
-      </select>
-    </div>
+    mounted && (
+      <div>
+        <select
+          className="rounded-full cursor-pointer inline-block sm:hidden border-2 border-slate-400"
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
+        >
+          <option value="system">&#127763;</option>
+          <option value="dark">&#127761;</option>
+          <option value="light">&#127765;</option>
+        </select>
+        <select
+          className="rounded-md cursor-pointer hidden sm:inline-block border-2 border-slate-700"
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
+        >
+          <option value="system">&#127763; System</option>
+          <option value="dark">&#127761; Dark</option>
+          <option value="light">&#127765; Light</option>
+        </select>
+      </div>
+    )
   );
 }
