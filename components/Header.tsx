140ec416b7951e4e1fafa0ea1fbe7fb6e122ad2a
Erwin <147875779+Erwinex@users.noreply.github.com>
2024-04-15 01:31:14 +0330

    Restyling the responsive bug on mobiles

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import HeaderItem from "./HeaderItem";
 
 export default function Header() {
   return (
-    <header className="flex p-6 lg:text-xl md:text-lg dark:bg-violet-800 bg-violet-300 font-bold items-center justify-center gap-32">
+    <header className="flex p-6 lg:text-xl md:text-lg dark:bg-violet-800 bg-violet-300 font-bold items-center sm:justify-center sm:gap-32 justify-between gap-0">
       <Suspense>
         <HeaderItem lable="Trending" param="fetchTrending" />
         <HeaderItem lable="Top Rated" param="fetchTopRated" />
