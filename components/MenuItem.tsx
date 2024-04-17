0f31b473b574c57388fd4cdd31878b1c1715f2d6
Erwin <147875779+Erwinex@users.noreply.github.com>
2024-04-18 01:19:42 +0330

    Making hambergur menu and adding special loading efect to the search section and fixing responsive bugs

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,11 +1,10 @@
 import { type MenuItemProp } from "@/types";
 import Link from "next/link";
 
-export default function MenuItem({ lable, address, Icon }: MenuItemProp) {
+export default function MenuItem({ lable, address, variant }: MenuItemProp) {
   return (
-    <Link href={address} className="hover:text-violet-500">
-      <Icon className="inline-block md:hidden text-4xl" />
-      <p className="uppercase hidden md:inline-block text-lg">{lable}</p>
+    <Link href={address} className={"hover:text-violet-500 " + variant}>
+      <p className="uppercase text-lg">{lable}</p>
     </Link>
   );
 }
